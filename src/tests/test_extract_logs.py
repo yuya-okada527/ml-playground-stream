10f63002fbb11aa3aa4c45ea86596c7e5b8f226c
yuya-okada527 <yuya.okada527@gmail.com>
2021-04-21 22:29:59 +0900

    add extract_logs test

---
+++ src/tests/test_extract_logs.py
@@ -0,0 +1,15 @@
+from app.main import extract_logs
+
+def test_extract_logs_not_json():
+
+    event = {
+        "attributes": {
+            "bucketId": "bucket",
+            "objectId": "not-json.txt"
+        }
+    }
+    context = None
+
+    result = extract_logs(event, context)
+
+    assert result is None
