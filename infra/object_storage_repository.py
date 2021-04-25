ecc17107225197197b3a4f9f2f7e5a26c4469881
yuya-okada527 <yuya.okada527@gmail.com>
2021-04-25 10:45:00 +0900

    fix comment position

--- infra/object_storage_repository.py
+++ infra/object_storage_repository.py
@@ -8,7 +8,7 @@ GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 
 class ObjectStorageRepository(abc.ABC):
 
-    def download_contents(self, bucket: str, key: str) -> str:
+    def download_contents(self, bucket_name: str, key: str) -> str:
         raise NotImplementedError()
 
 
@@ -21,9 +21,9 @@ class GcsRepository(ObjectStorageRepository):
         self.__buckets = {}
 
 
-    def download_contents(self, bucketName: str, key: str) -> str:
+    def download_contents(self, bucket_name: str, key: str) -> str:
 
-        bucket = self.__get_bucket(bucketName)
+        bucket = self.__get_bucket(bucket_name)
 
         blob = bucket.blob(key)
 
@@ -32,11 +32,11 @@ class GcsRepository(ObjectStorageRepository):
     def __init_bucket(self, bucket) -> None:
         self.__buckets[bucket] = self.__client.get_bucket(bucket)
 
-    def __get_bucket(self, bucketName: str) -> gcs.Bucket:
-        bucket = self.__buckets.get(bucketName)
+    def __get_bucket(self, bucket_name: str) -> gcs.Bucket:
+        bucket = self.__buckets.get(bucket_name)
         if not bucket:
-            self.__init_bucket(bucket)
-            bucket = self.__buckets[bucket]
+            self.__init_bucket(bucket_name)
+            bucket = self.__buckets[bucket_name]
 
         return bucket
 
