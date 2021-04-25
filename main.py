add7f928bbd63cc17b92f8d104eb71c3fdce4823
yuya-okada527 <yuya.okada527@gmail.com>
2021-04-25 10:42:19 +0900

    add if

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from infra.object_storage_repository import create_object_storage_repository
 def extract_logs(event, context):
 
     # ログ取得先をメッセージから取得
-    bucket = event["attributes"]["bucketId"]
+    bucket_name = event["attributes"]["bucketId"]
     object_key = event["attributes"]["objectId"]
 
     # JSON出ない場合、スキップ
@@ -15,7 +15,7 @@ def extract_logs(event, context):
 
     # データを取得
     data = repository.download_contents(
-        bucket=bucket,
+        bucket_name=bucket_name,
         key=object_key
     )
 
